603ff3d0541d230f977781f05344b698adf9d3cf
Jan Kolář <kolar-jan885@post.cz>
2022-04-05 19:19:48 +0200

    Finally added some buttons that look like a callendar - not let the css battle begin

--- react-callendar/src/components/Callendar.tsx
+++ react-callendar/src/components/Callendar.tsx
@@ -1,15 +1,58 @@
+import { throws } from "assert";
 import React from "react";
 import { Days, IDays } from "./Days";
 
 export class Callendar extends React.Component<ICallendarProps,ICallendarState>{
+    constructor(props : ICallendarProps){
+        super(props);
+        const year = props.year !== undefined? props.year : new Date().getFullYear();
+        const month = props.month !== undefined? props.month : new Date().getMonth();
+        this.state = {
+            year : year,
+            month : month,
+            callendarName : 'test-callendar',
+            days : new Array<IDays>(this.getDaysOfMonth(year,month))
+        };     
+    }
+
+    //for now just retrun number of days
+    getDaysOfMonth(year : number,monthNumber : number) : number{
+        return new Date(year,monthNumber,0).getDate();
+    }
+
+    renderDays() : void{
+        let newDays : Array<IDays> = new Array<IDays>();
+        for(let i = 0; i < this.state.days.length; i++){
+            newDays.push({dayNbr : i,dayEvents : undefined} as IDays);
+        }
+        this.setState({days : newDays});
+    }
+
+    componentDidMount(){
+        this.renderDays();
+    }
+
+    render(): React.ReactNode {
+        return(
+            <div className="callendar">
+                {this.state.days.map((val) =>{
+                    return <Days key={val.dayNbr} dayNbr={val.dayNbr} dayEvents={val.dayEvents} />
+                })}
+            </div>
+        );
+    }
     
 }
 
 interface ICallendarProps{
     //this will probably be empty
+    year?: number,
+    month? : number
 }
 
 export interface ICallendarState{
+    year : number,
+    month : number,
     callendarName : string,
     days : Array<IDays>
 }
--- react-callendar/src/components/Days.tsx
+++ react-callendar/src/components/Days.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { IEventProps } from "./Event";
+import '../css/callendarBtn.css';
 
-export class Days extends React.Component{
+export class Days extends React.Component<IDays,{}>{
     render(): React.ReactNode {
         return(
-            <></>
+            <button type="button" className="btn btn-info callendar-item">{this.props.dayNbr + 1}</button>
         )
     }
 }
 
 export interface IDays{
     dayNbr : number, //this should always be identifier
-    dayEvents : Array<IEventProps>
+    dayEvents? : Array<IEventProps>
 }
